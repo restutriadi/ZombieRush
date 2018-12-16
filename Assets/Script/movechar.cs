a3e60aa9e1bd4bfc7edf525959b41a924fd59d53
unknown <alfakatsuki@gmail.com>
2018-12-16 17:07:55 +0700

    add peluru

--- Assets/Script/movechar.cs
+++ Assets/Script/movechar.cs
@@ -12,45 +12,58 @@ public class movechar : MonoBehaviour {
 
     public KeyCode moveL;
     public KeyCode moveR;
+    public KeyCode Space;
 
     public float horizVel = 0;
     public int laneNum = 2;
     public string controlLocked = "n";
     public float horspd;
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
 	// Use this for initialization
 	void Start () {
         onGround = true;
-        //rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
         //rb.velocity = Vector3.forward * speed;
 	}
 
 	// Update is called once per frame
 	void Update () {
-
+        Physics.gravity = new Vector3(0, -1.0F, 0);
         Score.text = "Score: " + GM.coinTotal.ToString();
         horizVel = Input.GetAxis("Horizontal") * horspd;
         // Debug.log(horizVel);
-		GetComponent<Rigidbody>().velocity = new Vector3 (horizVel,0,4);
+		rb.velocity = new Vector3 (horizVel,0,4);
 
-		if ((Input.GetKeyDown(moveL)) && (laneNum>1) && (controlLocked == "n")){
-            horizVel = -2;
-            StartCoroutine(stopSlide());
-            laneNum -= 1;
-            controlLocked = "y";
+		if (!onGround){
+           rb.velocity = new Vector3(horizVel,4,0);
+            //onGround = false;
 		}
+        if(Input.GetKeyDown(Space)){
+            Fire();
 
-		if ((Input.GetKeyDown(moveR)) && (laneNum<3) && (controlLocked == "n")){
-            horizVel = 2;
-            StartCoroutine(stopSlide());
-            laneNum += 1;
-            controlLocked = "y";
-		}
+        }
 
-		//if (Input.GetKeyDown(moveU) && onGround){
-        //    rb.velocity (horizVel,4,0);
-        //    onGround = false;
-		//}
 	}
+
+
+    void Fire()
+{
+
+    
+    // Create the Bullet from the Bullet Prefab
+    var bullet = (GameObject)Instantiate (
+        bulletPrefab,
+        bulletSpawn.position,
+        bulletSpawn.rotation);
+
+    // bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+    bullet.GetComponent<Rigidbody>().velocity = new Vector3 (horizVel,0,10);
+
+    Destroy(bullet, 2.0f);
+}
 
 	void OnCollisionEnter(Collision other){
         if(other.gameObject.tag == "lethal"){
@@ -66,9 +79,9 @@ public class movechar : MonoBehaviour {
 	}
 
     void OnTriggerEnter(Collider other){
-        if(other.gameObject.name == "Gate"){
-            SceneManager.LoadScene("LvlComplete");
-        }
+        // if(other.gameObject.name == "Gate"){
+        //     SceneManager.LoadScene("LvlComplete");
+        // }
         if(other.gameObject.name == "Coin(Clone)"){
             Destroy(other.gameObject);
             GM.coinTotal += 1;
